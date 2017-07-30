0b0066c4e470594bdc12898e1d60a42fd7f5ae24
Cam Crews <camcrews@gmail.com>
2017-07-30 11:58:15 -0400

    Email check update, reformatting

--- ws-form/js/main.js
+++ ws-form/js/main.js
@@ -10,25 +10,58 @@ document.addEventListener("DOMContentLoaded",function(){
     // password check
     if( elPassword.value != "12345678" ) {
       elH1.innerText = "Oh no, bad password!!";
+    } else if( !/\d/.test(elUsername.value ) ) {
+      elH1.innerText = "Ooops, there was no number in your username... :'(";
+    } else if( !/^([a-z][a-z0-9\.]*)@((?:[a-z0-9]+\.)+[a-z]{2,3})$/i.test( elEmail.value ) ) {
+      elH1.innerText = "Your email looks off....";
     } else {
-      // password is okay.. let's check for a number
-      // in the username
-      let numberFound = false;
-      for(let i=0; i<elUsername.value.length; i++){
-        if( "0123456789".indexOf(elUsername.value[i]) > -1 ) {
-          numberFound = true;
-          break;
-        }
-      }
-      if( numberFound ) {
-        // elH1.innerText = "Welcome " + elUsername.value + "!";
-        elH1.innerText = `Welcome ${elUsername.value}!`;
-      } else {
-        elH1.innerText = "Ooops, there was no number in your username... :'(";
-      }
+       elH1.innerText = `Welcome ${elUsername.value}!`;
     }
+    // } else {
+    // //} else if( /\d/.test(elUsername.value) )
+    //   // password is okay.. let's check for a number
+    //   // in the username
+    //   let numberFound = false;
+    //   for(let i=0; i<elUsername.value.length; i++){
+    //     if( "0123456789".indexOf(elUsername.value[i]) > -1 ) {
+    //       numberFound = true;
+    //       break;
+    //     }
+    //   }
+    //   // email test with regular expressions
+    //   // must start with a letter
+    //   // then there to be letters-numbers or a dot zero or more times
+    //   // then an @ symbol
+    //   // then more letters-numbers...  and then a dot
+    //   // and then 2-3 letters.
+      
+    //   if( !/^([a-z][a-z0-9\.]*)@((?:[a-z0-9]+\.)+[a-z]{2,3})$/i.test( elEmail.value) ) {
+    //     elH1.innerText = "Your email looks off....";
+    //   }
+
+    //   // * - zero or many
+    //   // + - one or many
+    //   // ? - zero or one
+
+    //   // (?:)
+
+    //   if( numberFound ) {
+    //     // elH1.innerText = "Welcome " + elUsername.value + "!";
+    //     elH1.innerText = `Welcome ${elUsername.value}!`;
+    //   } else {
+    //     elH1.innerText = "Ooops, there was no number in your username... :'(";
+    //   }
+    // }
     console.log("Form Submitted");
+  });
+  // handle counters for bonus example
+  document.querySelectorAll(".counter button").forEach(function( el ) {
+    el.addEventListener("click",function(event){
+      console.log( event );
+    });
   });
 });
 //document.querySelector("form").onsubmit(function(){});
 
+//
+//Array.prototype.forEach.call(document.getElementsByClassName("counter"),function(el){ console.log( el ); });
